72f414d624ed544f24c423f85fb0269e3910addf
srankmeng <Jtfwu28AN>
2021-08-10 19:52:57 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,40 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+  const style = {
+    header: {
+      textAlign: 'center',
+      background: '#282c34',
+      color: '#fff',
+      position: 'fixed',
+      left: 0,
+      right: 0,
+      bottom: 0,
+    },
+    link: {
+      color: '#61dafb',
+    }
+  }
+  const [user, setUser] = useState([]);
+
+  useEffect(() => {
+    const init = async () => {
+      const { data } = await axios.get('https://gorest.co.in/public/v1/users');
+      setUser(data.data[0]);
+    };
+    init();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div>
+      <header style={style.header}>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          name: {user.name}
         </p>
         <a
-          className="App-link"
+           style={style.link}
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.body.appendChild(document.createElement("div"))
 );
 
 // If you want to start measuring performance in your app, pass a function
